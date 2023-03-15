8ad829810093a8f63443b69ae9d35d992951c527
azusaw <azusa0304@gmail.com>
2023-03-15 19:39:30 +0000

    update data

---
+++ data/_bk/continent_region.sql
@@ -0,0 +1,33 @@
+INSERT INTO disaster_continent (id) VALUES
+	 ('Africa'),
+	 ('Americas'),
+	 ('Asia'),
+	 ('Europe'),
+	 ('Oceania')
+;
+
+
+INSERT INTO disaster_region (id, continent) VALUES
+	 ('Western Africa','Africa'),
+	 ('Southern Africa','Africa'),
+	 ('Eastern Africa','Africa'),
+	 ('Northern Africa','Africa'),
+	 ('Middle Africa','Africa'),
+	 ('South America','Americas'),
+	 ('Northern America','Americas'),
+	 ('Caribbean','Americas'),
+	 ('Central America','Americas'),
+	 ('Western Asia','Asia'),
+	 ('Southern Asia','Asia'),
+	 ('Eastern Asia','Asia'),
+	 ('Central Asia','Asia'),
+	 ('South-Eastern Asia','Asia'),
+	 ('Western Europe','Europe'),
+	 ('Southern Europe','Europe'),
+	 ('Eastern Europe','Europe'),
+	 ('Northern Europe','Europe'),
+	 ('Australia and New Zealand','Oceania'),
+	 ('Melanesia','Oceania'),
+	 ('Micronesia','Oceania'),
+	 ('Polynesia','Oceania')
+;
