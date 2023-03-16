49ea18cc5776c92ce56c917a65ba6af18efaa221
azusaw <azusa0304@gmail.com>
2023-03-16 14:56:15 +0000

    create home page

--- disaster/urls.py
+++ disaster/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.disaster_list, name='home'),
+    path('', views.home, name='home'),
     path('all', views.disaster_list, name='all'),
     path('detail/<slug:disasterNo>/', views.disaster_detail, name='detail'),
     path('search', views.disaster_search, name='search'),
--- disaster/views.py
+++ disaster/views.py
@@ -3,15 +3,17 @@ from django.shortcuts import render, get_object_or_404
 from .models import Disaster, Type, Subgroup, Continent, Region
 
 
+def home(request):
+    return render(request, 'home.html')
+
+
 def disaster_list(request):
     disasters = Disaster.objects.all()
-
     return render(request, 'disaster_list.html', {'disasters': disasters})
 
 
 def disaster_detail(request, disasterNo):
     disaster = get_object_or_404(Disaster, disasterNo=disasterNo)
-
     return render(request, 'disaster_detail.html', {'disaster': disaster})
 
 
@@ -20,6 +22,5 @@ def disaster_search(request):
     subgroups = Subgroup.objects.all()
     continents = Continent.objects.all()
     regions = Region.objects.all()
-
     return render(request, 'disaster_search.html',
                   {'types': types, 'subgroups': subgroups, 'continents': continents, 'regions': regions})
