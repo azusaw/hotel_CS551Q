8dc889e0986e9ac68666337319e13857c4874192
azusaw <azusa0304@gmail.com>
2023-03-17 18:22:10 +0000

    modify settings for render: get css error

--- mysite/settings.py
+++ mysite/settings.py
@@ -115,7 +115,10 @@ USE_TZ = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
