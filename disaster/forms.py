dd2381dac8bb43a2f507c721611ae78e4e05a9ff
azusaw <azusa0304@gmail.com>
2023-03-27 18:07:34 +0100

    fix error of editing desaster

--- disaster/forms.py
+++ disaster/forms.py
@@ -10,111 +10,145 @@ class DisasterForm(forms.ModelForm):
         model = Disaster
         fields = '__all__'
 
+    # called on validation of the form
+    def clean(self):
+        # run the standard clean method first
+        cleaned_data = super(DisasterForm, self).clean()
+        print(cleaned_data)
+        for k, v in cleaned_data.items():
+            if v == '':
+                cleaned_data[k] = None
+        print(cleaned_data)
+
+        return cleaned_data
+
+    required_css_class = 'required'
+
     # Get options from database
     types = Type.objects.all()
     continents = Continent.objects.all()
     regions = Region.objects.all()
 
     disasterNo = forms.CharField(
+        label="Subtype",
         widget=forms.widgets.TextInput(attrs={'class': 'form-control col-sm-5'})
     )
     year = forms.CharField(
+        label="Year",
         widget=forms.widgets.NumberInput(attrs={'class': 'form-control col-sm-5'})
     )
     seq = forms.CharField(
-        widget=forms.widgets.TextInput(attrs={'class': 'form-control col-sm-5'})
+        label="Seq",
+        widget=forms.widgets.NumberInput(attrs={'class': 'form-control col-sm-5'})
     )
     group = forms.CharField(
+        label="Group",
         widget=forms.widgets.TextInput(attrs={'class': 'form-control col-sm-5'})
     )
     subgroup = forms.CharField(
+        label="Subgroup",
         widget=forms.widgets.TextInput(attrs={'class': 'form-control col-sm-5'})
     )
-    type = forms.fields.ChoiceField(
-        required=False,
-        choices=[('', 'All')] + [(item.id, item.id) for item in types],
+    type = forms.ModelChoiceField(
+        label="Type",
+        queryset=Type.objects.all(),
         widget=forms.widgets.Select(attrs={'class': 'form-control form-inline'})
     )
     subtype = forms.CharField(
         required=False,
+        label="Subtype",
         widget=forms.widgets.TextInput(attrs={'class': 'form-control col-sm-5'})
     )
     eventName = forms.CharField(
         required=False,
+        label="Event Name",
         widget=forms.widgets.TextInput(attrs={'class': 'form-control col-sm-5'})
     )
     iso = forms.CharField(
-        required=False,
+        label="ISO",
         widget=forms.widgets.TextInput(attrs={'class': 'form-control col-sm-5'})
     )
-    region = forms.fields.ChoiceField(
-        required=False,
-        choices=[('', 'All')] + [(item.id, item.id) for item in regions],
+    region = forms.ModelChoiceField(
+        label="Region",
+        queryset=Region.objects.all(),
         widget=forms.widgets.Select(attrs={'class': 'form-control col-sm-5'})
     )
-    continent = forms.fields.ChoiceField(
-        required=False,
-        choices=[('', 'All')] + [(item.id, item.id) for item in continents],
+    continent = forms.ModelChoiceField(
+        label="Continent",
+        queryset=Continent.objects.all(),
         widget=forms.widgets.Select(attrs={'class': 'form-control col-sm-5'})
     )
     country = forms.CharField(
-        required=False,
+        label="Country",
         widget=forms.widgets.TextInput(attrs={'class': 'form-control col-sm-5'})
     )
     location = forms.CharField(
-        required=False,
+        label="Location",
         widget=forms.widgets.TextInput(attrs={'class': 'form-control col-sm-5'})
     )
     startYear = forms.CharField(
         required=False,
+        label="Start Year",
         widget=forms.widgets.NumberInput(attrs={'class': 'form-control col-sm-5'})
     )
     startMonth = forms.CharField(
         required=False,
+        label="Start Month",
         widget=forms.widgets.NumberInput(attrs={'class': 'form-control col-sm-5'})
     )
     startDay = forms.CharField(
         required=False,
+        label="Start Day",
         widget=forms.widgets.NumberInput(attrs={'class': 'form-control col-sm-5'})
     )
     endYear = forms.CharField(
         required=False,
+        label="End Year",
         widget=forms.widgets.NumberInput(attrs={'class': 'form-control col-sm-5'})
     )
     endMonth = forms.CharField(
         required=False,
+        label="End Month",
         widget=forms.widgets.NumberInput(attrs={'class': 'form-control col-sm-5'})
     )
     endDay = forms.CharField(
         required=False,
+        label="End Day",
         widget=forms.widgets.NumberInput(attrs={'class': 'form-control col-sm-5'})
     )
     totalDeath = forms.CharField(
         required=False,
+        label="Total Deaths",
         widget=forms.widgets.NumberInput(attrs={'class': 'form-control col-sm-5'})
     )
     injured = forms.CharField(
         required=False,
+        label="Injured",
         widget=forms.widgets.NumberInput(attrs={'class': 'form-control col-sm-5'})
     )
     affected = forms.CharField(
         required=False,
+        label="Affected",
         widget=forms.widgets.NumberInput(attrs={'class': 'form-control col-sm-5'})
     )
     totalAffected = forms.CharField(
         required=False,
+        label="Total Affected",
         widget=forms.widgets.NumberInput(attrs={'class': 'form-control col-sm-5'})
     )
     totalDeaths = forms.CharField(
         required=False,
+        label="Total Deaths",
         widget=forms.widgets.NumberInput(attrs={'class': 'form-control col-sm-5'})
     )
     homeless = forms.CharField(
         required=False,
+        label="Homeless",
         widget=forms.widgets.NumberInput(attrs={'class': 'form-control col-sm-5'})
     )
     damageCost = forms.CharField(
         required=False,
+        label="Damage Cost",
         widget=forms.widgets.NumberInput(attrs={'class': 'form-control col-sm-5'})
     )
 
--- disaster/models.py
+++ disaster/models.py
@@ -8,35 +8,44 @@ class Disaster(models.Model):
     group = models.TextField()
     subgroup = models.TextField()
     type = models.ForeignKey('Type', on_delete=models.CASCADE)
-    subtype = models.TextField(null=True)
-    eventName = models.TextField(null=True)
+    subtype = models.TextField(null=True, blank=True)
+    eventName = models.TextField(null=True, blank=True)
     country = models.TextField()
     iso = models.TextField()
     region = models.ForeignKey('Region', on_delete=models.CASCADE)
     continent = models.ForeignKey('Continent', on_delete=models.CASCADE)
     location = models.TextField()
-    startYear = models.IntegerField(null=True)
-    startMonth = models.IntegerField(null=True)
-    startDay = models.IntegerField(null=True)
-    endYear = models.IntegerField(null=True)
-    endMonth = models.IntegerField(null=True)
-    endDay = models.IntegerField(null=True)
-    totalDeaths = models.IntegerField(null=True)
-    injured = models.IntegerField(null=True)
-    affected = models.IntegerField(null=True)
-    totalAffected = models.IntegerField(null=True)
-    homeless = models.IntegerField(null=True)
-    damageCost = models.IntegerField(null=True)
+    startYear = models.IntegerField(null=True, blank=True)
+    startMonth = models.IntegerField(null=True, blank=True)
+    startDay = models.IntegerField(null=True, blank=True)
+    endYear = models.IntegerField(null=True, blank=True)
+    endMonth = models.IntegerField(null=True, blank=True)
+    endDay = models.IntegerField(null=True, blank=True)
+    totalDeaths = models.IntegerField(null=True, blank=True)
+    injured = models.IntegerField(null=True, blank=True)
+    affected = models.IntegerField(null=True, blank=True)
+    totalAffected = models.IntegerField(null=True, blank=True)
+    homeless = models.IntegerField(null=True, blank=True)
+    damageCost = models.IntegerField(null=True, blank=True)
 
 
 class Continent(models.Model):
     id = models.TextField(primary_key=True)
+
+    def __str__(self):
+        return self.id
 
 
 class Region(models.Model):
     id = models.TextField(primary_key=True)
     continent = models.ForeignKey('Continent', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.id
+
 
 class Type(models.Model):
     id = models.TextField(primary_key=True)
+
+    def __str__(self):
+        return self.id
