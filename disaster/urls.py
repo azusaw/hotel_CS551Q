89ac1bd4822c9350b08f4c0ce04b283b15aa2860
azusaw <azusa0304@gmail.com>
2023-03-27 18:19:46 +0100

    modify create data button and url

--- disaster/urls.py
+++ disaster/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('detail/<slug:disasterNo>/', views.disaster_detail, name='detail'),
     path('search', views.search, name='search'),
     path('comparison', views.comparison, name='comparison'),
-    path('disaster_new/', views.disaster_new, name='disaster_new'), #'bear_new/'是地址名，views.bear_new是views.py中定义的def的名字，最终返回的是views.py中def的render的模板
+    path('disaster/new', views.disaster_new, name='disaster_new'),
     path('disaster/<slug:disasterNo>/edit', views.disaster_edit, name='disaster_edit'),
-    path('disaster/<slug:disasterNo>/delete',views.disaster_delete, name='disaster_delete'),
+    path('disaster/<slug:disasterNo>/delete', views.disaster_delete, name='disaster_delete'),
 ]
