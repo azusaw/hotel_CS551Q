ae695d42b5ddb5334af132cc65ebe5b60cd8939d
azusaw <azusa0304@gmail.com>
2023-03-30 13:01:19 +0100

    edit page name comparison to chart

--- disaster/urls.py
+++ disaster/urls.py
@@ -7,10 +7,9 @@ urlpatterns = [
     path('all', views.disaster_list, name='all'),
     path('detail/<slug:disasterNo>/', views.disaster_detail, name='detail'),
     path('search', views.search, name='search'),
-    path('comparison', views.comparison, name='comparison'),
     path('new', views.disaster_new, name='disaster_new'),
     path('edit/<slug:disasterNo>', views.disaster_edit, name='disaster_edit'),
     path('delete/<slug:disasterNo>', views.disaster_delete, name='disaster_delete'),
-    path('chart/', views.chart, name='chart'),
+    path('chart', views.chart, name='chart'),
 
 ]
