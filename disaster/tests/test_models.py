ad8f37026aa58f88349424f4ec252f544b6eff63
saki <kksakikk@outlook.com>
2023-03-30 21:05:57 +0100

    update tests

--- disaster/tests/test_models.py
+++ disaster/tests/test_models.py
@@ -5,24 +5,28 @@ class DisasterModelTest(TestCase):
 
     fixtures = ['disaster.json']
 
+    # 1
     def test_disaster(self):
         disaster = Disaster.objects.get(disasterNo="2020-0055-WEF")
         self.assertEqual(disaster.year, 2020)
         disasters =Disaster.objects.all()
         self.assertEqual(disasters.count(), 1)
 
+    # 2
     def test_continent(self):
         continent = Continent.objects.get(pk="Asia")
         self.assertEqual(continent.id, "Asia")
         continents =Continent.objects.all()
         self.assertEqual(continents.count(), 1)
 
+    # 3
     def test_region(self):
         region = Region.objects.get(pk="SouthernAsia")
         self.assertEqual(region.continent.id, "Asia") # assert continent object
         regions = Region.objects.all()
         self.assertEqual(regions.count(), 1)
 
+    # 4
     def test_type(self):
         disaster_type = Type.objects.get(pk="Flood")
         self.assertEqual(disaster_type.id, "Flood")
--- disaster/tests/test_views.py
+++ disaster/tests/test_views.py
@@ -7,6 +7,7 @@ class DisasterViewsTest(TestCase):
     fixtures = ['disaster.json']
 
     # test the home page
+    # 1
     def test_home(self):
         client = Client()
         response = client.get('')
@@ -14,12 +15,14 @@ class DisasterViewsTest(TestCase):
         self.assertTemplateUsed(response, 'home.html')
         self.assertContains(response, "Disasters in the world 1970 - 2021")
 
+    # 2
     def test_views_home_use_correct_template(self):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code,200)
         self.assertTemplateUsed(response, 'home.html')
 
     # test the all page
+    # 3
     def test_disaster_list(self):
         client = Client()
         response = client.get('/all')
@@ -28,12 +31,14 @@ class DisasterViewsTest(TestCase):
         self.assertContains(response, "500")
         self.assertContains(response, "Asia")
 
+    # 4
     def test_views_all_use_correct_template(self):
         response = self.client.get(reverse('all'))
         self.assertEqual(response.status_code,200)
         self.assertTemplateUsed(response, 'all.html')
 
     # test the disaster_detail page
+    # 5
     def test_disaster_detail(self):
         client = Client()
         response = client.get('/detail/2020-0055-WEF/')
@@ -43,6 +48,7 @@ class DisasterViewsTest(TestCase):
         self.assertContains(response, "7876")
 
     # test the search page
+    # 6
     def test_search(self):
         client = Client()
         response = client.get('/search')
@@ -50,8 +56,18 @@ class DisasterViewsTest(TestCase):
         self.assertTemplateUsed(response, 'search.html')
         self.assertContains(response, "Search Data by Conditions")
 
+
+    # 7
     def test_views_serch_use_correct_template(self):
         response = self.client.get(reverse('search'))
         self.assertEqual(response.status_code,200)
         self.assertTemplateUsed(response, 'search.html')
-    
+
+    # 8
+    def test_new(self):
+        client = Client()
+        response = client.get('/new')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'disaster_edit.html')
+        self.assertContains(response, "Edit:")
+
